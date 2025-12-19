899861f2b75f9b8f32bb97479968206fcb1e2547
Rafał Stróżyk <rafal.strozyk@onet.pl>
2025-12-20 00:54:30 +0100

    fix(workflow): change scp to rsync

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p>Co się gapisz parówo dęta</p>
 </template>
 
 <style scoped></style>
