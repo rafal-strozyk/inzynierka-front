ae2d24b215365df4545cd70b1ba090c5e7aa7392
Rafał Stróżyk <rafal.strozyk@onet.pl>
2025-12-19 23:36:07 +0100

    feat(config): add plugin to make index.html into a laravel view

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig, PluginOption} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+function renameIndexPluigin(newFilename: string | undefined): PluginOption {
+  if( !newFilename ) {
+    return false;
+  }
+  return {
+    name: "renameIndex",
+    enforce: "post",
+    generateBundle(_, bundle) {
+      const indexHtml = bundle["index.html"];
+      indexHtml.fileName = newFilename
+    }
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    renameIndexPluigin("../resources/views/frontend/index.blade.php"),
     vueDevTools(),
   ],
   resolve: {
