f7757ed96450862a06838f2772596e67fc4a5e22
Rafał Stróżyk <rafal.strozyk@onet.pl>
2026-02-09 18:48:21 +0100

    refactor(api-helper): simplify class to object

--- frontend/src/helpers/api-helper.ts
+++ frontend/src/helpers/api-helper.ts
@@ -1,26 +1,14 @@
-import Axios, { type AxiosInstance, type AxiosRequestConfig } from "axios";
+import Axios from "axios";
 
-class ApiHelper {
-  private axios: AxiosInstance;
+const apiHelper = Axios.create({
+  baseURL: "/api",
+});
 
-  constructor() {
-    this.axios = Axios.create({
-      baseURL: "/api",
-    });
+apiHelper.interceptors.request.use(function (config) {
+  config.headers.Authorization = `Bearer ${
+    localStorage.getItem("token") ?? sessionStorage.getItem("token") ?? ""
+  }`;
+  return config;
+});
 
-    this.axios.interceptors.request.use(function (config) {
-      config.headers.Authorization = `Bearer ${localStorage.getItem("token") ?? sessionStorage.getItem("token") ?? ""}`;
-      return config;
-    });
-  }
-
-  get(url: string, config?: AxiosRequestConfig) {
-    return this.axios.get(url, config);
-  }
-
-  post(url: string, data?: unknown, config?: AxiosRequestConfig) {
-    return this.axios.post(url, data, config);
-  }
-}
-
-export default new ApiHelper();
+export default apiHelper;
