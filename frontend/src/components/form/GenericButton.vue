42e6ff48a3500474e3b20dd8d54748a3fecbdb8b
Rafał Stróżyk <rafal.strozyk@onet.pl>
2026-02-10 01:06:55 +0100

    feat(buttons): expand generioc button's features

--- frontend/src/components/form/GenericButton.vue
+++ frontend/src/components/form/GenericButton.vue
@@ -1,34 +1,61 @@
 <template>
   <button
-    @click.prevent="props.callback()"
-    type="button"
-    class="cursor-pointer inline-flex w-full items-center justify-center rounded-lg px-5 py-2.5 text-sm font-medium text-white focus:outline-none focus:ring-4 sm:w-auto"
-    :class="buttonTypeStyling[props.type]"
+    @click="props.callback?.()"
+    :type="props.type"
+    class="cursor-pointer inline-flex w-full items-center justify-center rounded-lg px-5 text-sm font-medium text-white focus:outline-none focus:ring-4 sm:w-auto"
+    :disabled="props.isSending"
+    :class="[buttonTypeStyling[props.variant], isSending ? 'pointer-events-none py-1.5' : 'py-2.5']"
   >
-    <inline-svg v-if="props.iconPath" class="-ms-0.5 me-1.5 size-5" :src="props.iconPath">
-    </inline-svg>
-    <slot></slot>
+    <template v-if="isSending">
+      <svg
+        aria-hidden="true"
+        class="size-7 text-neutral-tertiary animate-spin brightness-200 fill-primary-300 text-gray-700 dark:text-gray-700 dark:fill-primary-200"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </template>
+    <template v-else>
+      <inline-svg v-if="props.iconPath" class="-ms-0.5 me-1.5 size-5" :src="props.iconPath">
+      </inline-svg>
+      <slot></slot>
+    </template>
   </button>
 </template>
 <script setup lang="ts">
-type GenericButtonTypes = "primary" | "danger";
+import type { ModalCallbackType } from "@/types/modal.ts";
 
 type GenericButtonProps = {
-  type?: GenericButtonTypes;
+  type?: "button" | "submit" | "reset";
+  variant?: ModalCallbackType;
   iconPath?: `/${string}`;
-  callback: () => unknown;
+  isSending?: boolean;
+  callback?: () => unknown;
 };
 
 const buttonTypeStyling = {
   primary:
     "bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
+  ghost:
+    "bg-gray-700 hover:bg-gray-800 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800",
   danger:
     "bg-danger-700 hover:bg-danger-800 focus:ring-danger-300 dark:bg-danger-600 dark:hover:bg-danger-700 dark:focus:ring-danger-800",
-} satisfies Record<GenericButtonTypes, string>;
+} satisfies Record<ModalCallbackType, string>;
 
 const props = withDefaults(defineProps<GenericButtonProps>(), {
-  type: "primary",
-  callback: () => {},
+  type: "button",
+  variant: "primary",
+  isSending: false,
 });
 </script>
 <style scoped></style>
